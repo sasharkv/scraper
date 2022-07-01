53c115a310fa3463a82c785fb15705430a934b18
sasha radkova <sasharadkova@outlook.com>
2022-07-01 13:49:50 +0200

    added a method for getting the HtmlDocument

--- scraper/Program.cs
+++ scraper/Program.cs
@@ -16,12 +16,31 @@ using HtmlAgilityPack;
 
 string url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html";
 
-HtmlWeb web = new HtmlWeb();
+/* HtmlWeb web = new HtmlWeb();
 
 // load connects to the site and converts it into an HtmlDocument object (it's kind of like XML) which is ready to be queried
 HtmlDocument doc = web.Load(url);
 // DocumentNode gets you the complete document
-Console.Write(doc.DocumentNode.InnerText);
+Console.Write(doc.DocumentNode.InnerHtml); */
+
+
+// a method for getting the document
+static HtmlDocument GetDocument(string url)
+{
+    HtmlWeb web = new HtmlWeb();
+    HtmlDocument doc = web.Load(url);
+    return doc;
+}
+
+
+// get the page as an HtmlDocument
+HtmlDocument doc = GetDocument(url);
+
+// now we want to extract all the links from that page
+
+
+
+
 
 
 

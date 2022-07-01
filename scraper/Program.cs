525c8a2f697dc29dd305104af4db0aa139da865d
sasha radkova <sasharadkova@outlook.com>
2022-07-01 13:28:25 +0200

    gets the HtmlDocument of  a given URL

--- scraper/Program.cs
+++ scraper/Program.cs
@@ -1,7 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// books.toscrape.com
 
 // Scraping exercise:
-// 1. Get all links
-// 2. Process all links to get book details
-// 3. Export CSV
+// STEP 1. Get all links
+// STEP 2. Process all links to get book details
+// STEP 3. Export CSV
+
+// get the page
+// get an object of that page - this is parsing
+// get exactly the information you are looking for - querying
+// (we'll be looking for booktitle and price)
+// CSV (in real life it'll be a db)
+
+
+using HtmlAgilityPack;
+
+string url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html";
+
+HtmlWeb web = new HtmlWeb();
+
+// load connects to the site and converts it into an HtmlDocument object (it's kind of like XML) which is ready to be queried
+HtmlDocument doc = web.Load(url);
+// DocumentNode gets you the complete document
+Console.Write(doc.DocumentNode.InnerText);
+
+
+
+
+
+
 

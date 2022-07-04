9bf1db6f58a0e8ad0d5002a2dc593a086563fe1f
sasha radkova <sasharadkova@outlook.com>
2022-07-04 09:37:45 +0200

    added a method to extract the price from a raw string with regex

--- scraper/Program.cs
+++ scraper/Program.cs
@@ -14,6 +14,7 @@
 
 using HtmlAgilityPack;
 using scraper;
+using System.Text.RegularExpressions;
 
 string url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html";
 
@@ -55,6 +56,36 @@ static List<string> GetBookLinks(string url)
        links.Add(link);
     }
     return links;
+}
+
+List<Book> GetBooks(List<string> links)
+{
+    List<Book> books = new List<Book>();
+    foreach (string link in links) // this tim this link actually represents the book link
+    {
+        var doc = GetDocument(link);
+        var book = new Book();
+        // this time we only need 1 node - the title, and we assign it to a property of a newly created Book object
+        book.Title = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
+
+        // because price on the website is $45.17, we need to format the $ out
+
+        string xpath = "//*[@class=\"col - sm - 6 product_main\"]/*[@class=\"price_color\"]";
+        string price_raw = doc.DocumentNode.SelectSingleNode(xpath).InnerText; // this will be the entire string
+    }
+}
+
+// a function to extract the price
+
+static double ExtractPrice(string raw)
+{
+    // \d = means keep the decimal; \. means keep the . (escape the . because otherwise it means "everything") and , 
+    var reg = new Regex(@"[\d\.,]", RegexOptions.Compiled); // RegexOptions.Compiled makes it a bit faster
+    var m = reg.Match(raw); // look for a match
+    if (!m.Success) // handle exception if thereno match
+        return 0;
+
+    return Convert.ToDouble(m.Value); //remember to convert the string to double
 }
 
 
